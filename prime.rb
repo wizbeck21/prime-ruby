56943507f0547d868c2ccb5d1155ac7608393059
Wesley <wes.beck21@outlook.com>
2020-11-13 20:51:31 -0800

    commit tree 8cc9bdabbfa0e008da6357c1fc8a20ad60b6fa52

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+ def prime?(number)
+     return false if number < 2
+     n = 2
+     while n < number
+         return false if number % n == 0
+         n += 1
+     end
+     true
+ end
